d9922ee009b9252ee3dcd976f0011e434513b6e9
Avo-Keofteian <avokeofteian@gmail.com>
2020-04-25 17:34:58 +0300

    feat: simple lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Sample from './components/sample/sample.component';
-import UserApiCall from './components/users/users.component';
+// import UserApiCall from './components/users/users.component';
+const UserApiCall = lazy(() => import("./components/users/users.component"));
 
 function App() {
   return (
     <div>
       <Sample />
-      <UserApiCall />
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserApiCall />
+      </Suspense>
     </div>
   );
 }
--- src/components/users/users.component.tsx
+++ src/components/users/users.component.tsx
@@ -2,10 +2,8 @@ import React, { FC, useEffect } from 'react';
 // @ts-ignore
 import { connect } from 'react-redux';
 import { IAppState } from '../../redux/root-reducer';
-
-import { fetchUsers } from '../../redux/users/users.actions'
-
-import { Spinner } from 'react-bootstrap';
+import { fetchUsers } from '../../redux/users/users.actions';
+import Loader from '../../components/loader/loader.componet';
 
 interface ObjectProps {
     loading: Boolean;
@@ -19,15 +17,17 @@ interface Props {
 }
 
 const UsersApiCall: FC<Props> = ({ usersData, setUsers }) => {
-    console.log('----LOADING');
-    console.log(usersData.loading);
+
+    useEffect(() => {
+        setUsers()
+    }, []);
 
     return (
         <>
-            <button onClick={() => setUsers()}>Get Api</button>
+            {/* <button onClick={() => setUsers()}>Get Api</button> */}
 
             {usersData.loading &&
-                <Spinner animation="border" variant="dark" />
+                <Loader />
             }
 
             {usersData.error ? <p>Something went wrong please try again later</p> : null}
